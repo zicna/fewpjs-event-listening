294fe9c380e8d0cc1e1b1c9e2f86580deeca13a7
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-08-20 15:37:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+
+const input = document.getElementsByTagName('input')[0]
+
 function addingEventListener() {
+    input.addEventListener('click', function(event){
+        alert('I was clicked');
+    })
 }
